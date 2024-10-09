0e28edd126eb90fd9d3629e11caf94a0427dfdb9
milayg <galbraithmilay@gmail.com>
2024-10-09 09:58:28 -0700

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,11 @@ public class Player_movement : MonoBehaviour
     // TODO: Let player sprint (Done I think...), lock player movement while in dialogue (TO DO), make camera follow the player (Done)
     //       Also need a walk cycle animation once we have a design for the protag (I don't think that has to be in code) (sprint and idle animation optional)
 
-    public float speed = 5f;
-    public float sprint = 10f;
-    public float playerSpeed;
+    public float speed = 5f; // Normal speed.
+    public float sprint = 10f; // add on to speed when pressing the left shift button.
+    public float playerSpeed; // playerSpeed, calcuated using sprint and/or speed.
     public Rigidbody2D player;
-    public Animator playerAnimator;
+    public Animator playerAnimator; //controls animation speed, might not be needed, as sprite may be too quick to notice.
 
     Vector2 velocity;
 

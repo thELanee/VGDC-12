7bbe2a67299e97c7603fe179b7f1615be6671195
Ryan Luutuyen <rjluutuyenc@gmail.com>
2024-10-22 00:36:12 -0700

    Progressed on menu animations
    The menu animates with several bugs, (pages not showing right sometimes during animations).

--- Assets/Scripts/PageFlipping.cs
+++ Assets/Scripts/PageFlipping.cs
@@ -7,6 +7,7 @@ public class PageFlipping : MonoBehaviour
 {
     public GameObject page;
     int curPage;
+    public GameObject text2;
     public GameObject text3;
     public GameObject tab3;
 
@@ -19,14 +20,42 @@ public class PageFlipping : MonoBehaviour
         int.TryParse(page.transform.name.Substring(1), out PauseScript.curPage);
         switch (PauseScript.curPage)
         {
+            case 1:
+                if(curPage == 1)
+                {
+                    PauseScript.curPage = 1;
+                }
+                else if(curPage == 2)
+                {
+                    PauseScript.mAnimator2.SetTrigger("Close");
+                    PauseScript.tAnimator2.SetTrigger("Close");
+                    StartCoroutine(Wait(page));
+                }
+                else if(curPage == 3)
+                {
+                    PauseScript.mAnimator2.SetTrigger("Close");
+                    PauseScript.mAnimator3.SetTrigger("Close");
+                    PauseScript.tAnimator2.SetTrigger("Close");
+                    PauseScript.tAnimator3.SetTrigger("Close");
+                    StartCoroutine(Wait(page));
+                }
+                break;
             case 2:
                 if(curPage == 1)
                 {
                     PauseScript.mAnimator2.SetTrigger("Open");
+                    PauseScript.tAnimator2.SetTrigger("Open");
                     StartCoroutine(Wait(page));
-                }else if(curPage == 2)
+                }
+                else if(curPage == 2)
                 {
                     PauseScript.curPage = 2;
+                }
+                else if(curPage == 3)
+                {
+                    PauseScript.mAnimator3.SetTrigger("Close");
+                    PauseScript.tAnimator3.SetTrigger("Close");
+                    StartCoroutine(Wait(page));
                 }
                 break;
             case 3:
@@ -34,6 +63,7 @@ public class PageFlipping : MonoBehaviour
                 {
                     PauseScript.mAnimator2.SetTrigger("Open");
                     PauseScript.mAnimator3.SetTrigger("Open");
+                    PauseScript.tAnimator2.SetTrigger("Open");
                     PauseScript.tAnimator3.SetTrigger("Open");
                     StartCoroutine(Wait(page));
                 }else if(curPage == 2)
@@ -53,7 +83,30 @@ public class PageFlipping : MonoBehaviour
 
     IEnumerator Wait(GameObject page)
     {
-        if(PauseScript.curPage == 3)
+        if(curPage == 1 && PauseScript.curPage == 2)
+        {
+            text2.SetActive(false);
+        }
+        else if(curPage == 1 && PauseScript.curPage == 3)
+        {
+            text2.SetActive(false);
+            text3.SetActive(false);
+            page.transform.SetAsLastSibling();
+        }
+        else if(curPage == 2 && PauseScript.curPage == 3)
+        {
+            text3.SetActive(false);
+        }
+        else if(curPage == 2 && PauseScript.curPage == 1)
+        {
+            text2.SetActive(false);
+        }
+        else if(curPage == 3 && PauseScript.curPage == 1)
+        {
+            text2.SetActive(false); 
+            text3.SetActive(false);
+        }
+        else if(curPage == 3 && PauseScript.curPage == 2)
         {
             text3.SetActive(false);
         }
@@ -61,7 +114,29 @@ public class PageFlipping : MonoBehaviour
         tab3.transform.SetAsLastSibling();
         page.transform.SetAsLastSibling();
         yield return new WaitForSecondsRealtime(0.6f);
-        if (PauseScript.curPage == 3)
+        if (curPage == 1 && PauseScript.curPage == 2)
+        {
+            text2.SetActive(true);
+        }
+        else if (curPage == 1 && PauseScript.curPage == 3)
+        {
+            text2.SetActive(true);
+            text3.SetActive(true);
+        }
+        else if (curPage == 2 && PauseScript.curPage == 3)
+        {
+            text3.SetActive(true);
+        }
+        else if (curPage == 2 && PauseScript.curPage == 1)
+        {
+            text2.SetActive(true);
+        }
+        else if (curPage == 3 && PauseScript.curPage == 1)
+        {
+            text2.SetActive(true);
+            text3.SetActive(true);
+        }
+        else if (curPage == 3 && PauseScript.curPage == 2)
         {
             text3.SetActive(true);
         }
